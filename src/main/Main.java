b7e7579d01a75fb296072e7018a0ec010967f69e
Hamdan Javeed <hamdan@hamdanjaveed.com>
2013-05-08 19:55:52 -0400

    updated vm arguments, window now displays

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,19 +1,26 @@
 package main;
 
+import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.ScalableGame;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class Main extends StateBasedGame {
 
-	private static final String GAME_NAME;
+	private static final String  GAME_NAME     = "Bionic Quacker";
+	private static final int     GAME_WIDTH    = 1280;
+	private static final int     GAME_HEIGHT   = 720;
+	private static final boolean MOUSE_GRABBED = false;
+	private static final int     TARGET_FPS    = 60;
 
-	static {
-		GAME_NAME = "Bionic Quacker";
-	}
-
-	public static void main(String[] args) {
-		new Main();
+	public static void main(String[] args) throws SlickException {
+		AppGameContainer appGameContainer = new AppGameContainer(new ScalableGame(new Main(), GAME_WIDTH, GAME_HEIGHT));
+		appGameContainer.setDisplayMode(GAME_WIDTH, GAME_HEIGHT, false);
+		appGameContainer.setMouseGrabbed(MOUSE_GRABBED);
+		appGameContainer.setTargetFrameRate(TARGET_FPS);
+		appGameContainer.setTitle(GAME_NAME);
+		appGameContainer.start();
 	}
 
 	public Main() {
